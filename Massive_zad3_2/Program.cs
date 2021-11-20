01c67912f9b9c1164be30ce75a342730742bf926
nvrsk <nvrsk@DESKTOP-1RBTMCR>
2021-11-20 20:52:15 +0300

    Добавьте файлы проекта.

---
+++ Massive_zad3_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Massive_zad3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите массив, состоящий из 8 элементов, через пробел:");
+            var array = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i]<15)
+                {
+                    array[i] *= 2;
+                }
+            }
+            Console.WriteLine($"Изменненый массив: {string.Join(',', array)}");
+                
+        }
+    }
+}
